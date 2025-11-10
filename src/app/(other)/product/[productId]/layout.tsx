cdc56c0d8c1e867bce28372742b4b70d02c600ae
liuguanghui <2285190803@qq.com.com>
2025-11-10 23:09:09 +0800

    feat: Reorganize routes with auth and other layouts

---
+++ src/app/(other)/product/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+export default function DetailProductLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>详情产品页布局</h1>
+      {children}
+    </div>
+  );
+}
