882b0ed76f7817e7f1349b2f5b2d6e79df999d60
liuguanghui <2285190803@qq.com.com>
2025-11-12 22:59:50 +0800

    feat: Add metadata to about and product detail pages

--- src/app/(other)/about/page.tsx
+++ src/app/(other)/about/page.tsx
@@ -1,3 +1,8 @@
-export default function about(){
-  return <h1>关于我们页面</h1>
+export const metadata = {
+  title: "关于我们",
+  description: "这是关于我们页面的描述",
+};
+
+export default function about() {
+  return <h1>关于我们页面</h1>;
 }
--- src/app/(other)/product/[productId]/page.tsx
+++ src/app/(other)/product/[productId]/page.tsx
@@ -1,8 +1,16 @@
-export default async function ProductDetail({
-  params,
-}: {
+export type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+// 动态元数据
+export async function generateMetadata({ params }: Props) {
+  const { productId } = await params;
+  return {
+    title: `产品详情页：iphone ${productId}`,
+  };
+}
+
+export default async function ProductDetail({ params }: Props) {
   const { productId } = await params;
   return <h1>产品详情页：{productId}</h1>;
 }
