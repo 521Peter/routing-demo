1947fc918fa1dd88f3a30202fabd1e4c1d416dc7
liuguanghui <2285190803@qq.com.com>
2025-11-17 22:45:27 +0800

    feat: Add global error handling and product error page

--- src/app/(other)/product/[productId]/layout.tsx
+++ src/app/(other)/product/[productId]/layout.tsx
@@ -3,6 +3,9 @@ export default function DetailProductLayout({
 }: {
   children: React.ReactNode;
 }) {
+  if (Math.random() > 0.5) {
+    throw new Error("出错了");
+  }
   return (
     <div>
       <h1>详情产品页布局</h1>
