07d811d71590972c587d209c05b33d1f16ebf9b2
liuguanghui <2285190803@qq.com.com>
2025-11-18 22:51:56 +0800

    feat: Add login page with conditional dashboard rendering

---
+++ src/app/(other)/complex-dashboard/@login/page.tsx
@@ -0,0 +1,5 @@
+import Card from "@/app/components/card";
+
+export default function Login() {
+  return <Card text="请先登录"></Card>;
+}
--- src/app/(other)/complex-dashboard/layout.tsx
+++ src/app/(other)/complex-dashboard/layout.tsx
@@ -2,16 +2,22 @@ export default function ComplexDashboardLayout({
   children,
   users,
   notionfication,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   notionfication: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLogin = true;
+
+  return isLogin ? (
     <div>
       <div>{children}</div>
       <div>{users}</div>
       <div>{notionfication}</div>
     </div>
+  ) : (
+    <div>{login}</div>
   );
 }
